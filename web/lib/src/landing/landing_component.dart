2e848d5cac6138342a8e7187db770ecce2c4f305
Yogesh Surendran <37717@hexaware.com>
2019-01-02 21:36:32 +0530

    add form using material components.

--- web/lib/src/landing/landing_component.dart
+++ web/lib/src/landing/landing_component.dart
@@ -1,3 +1,5 @@
+import 'dart:html' show window;
+
 import 'package:angular/angular.dart';
 import 'package:angular_components/focus/focus.dart';
 import 'package:angular_components/material_input/material_input.dart';
@@ -16,13 +18,35 @@ import 'package:angular_forms/angular_forms.dart';
     MaterialButtonComponent,
     MaterialTooltipTargetDirective,
   ],
+  pipes: [JsonPipe],
   styleUrls: [
     'package:angular_components/css/mdc_web/card/mdc-card.scss.css',
     'landing_component.scss.css'
   ],
   preserveWhitespace: true,
-  // encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.None,
 )
 class LandingComponent {
-  
+  ControlGroup registerForm;
+  // NgFormModel fd;
+
+  LandingComponent() {
+    registerForm = FormBuilder.controlGroup({
+      'username': ['', Validators.required],
+      'email': ['', Validators.required],
+      'password': ['', Validators.required],
+      'passwordConfirmation': ['', Validators.required],
+    });
+  }
+
+  doRegister(e) {
+    // rf.isValid
+    window.console.log("gets called when submitted....");
+    window.console.log(e);
+    print("gets called when submitted....");
+  }
+
+  Map<String, dynamic> get value {
+    return registerForm.value;
+  }
 }
